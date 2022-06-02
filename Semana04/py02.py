cfb8091f8ec4919f0e2ec5c2a61495017347936b
João <joaoandrade.dtna@gmail.com>
2022-06-01 21:01:11 -0300

    Upload codes - Python Tutorial for Beginners 3: Strings - Working with Numeric Data

---
+++ Semana04/py02.py
@@ -0,0 +1,39 @@
+num_int = 3
+num_float = 3.14
+
+print(type(num_int))
+print(type(num_float))
+
+#Operações aritiméticas
+
+print(3+2)#Soma
+print(4-2)#Subtração
+print(3*2)#Multiplicação
+print(3/2)#Divisão comum
+print(7//3)#Retorna o valor da divisão arredondado para baixo
+print(5%3)#Módulo
+print(3**2)#Potenciação
+
+print(((3+2)*4 + 3**(3+1))+10)#Operações com prioridade
+
+#Funções
+
+print(round((3+2.423),2))#Arredondamento com 2 casas decimais
+print(abs(-3))#Módulo
+
+#Operações booleanas
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)#Operador igual
+print(num_1 != num_2)#Operador diferente
+print(num_1 >= 3)#Operador maior/igual
+print(num_1 < num_2)#Operador menor
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+print(int(num_1) + int(num_2))#Casting de variavéis
+
